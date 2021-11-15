fa5397c456eb6af742f1e84777509f22cc6b7873
danyu rajbahak <rajbahakdanyu@gmail.com>
2021-11-15 16:37:13 +0545

    Added book id

--- src/borrow_book.rs
+++ src/borrow_book.rs
@@ -4,8 +4,6 @@ use crate::utils::read_input;
 use std::fs;
 
 pub fn borrow_book() {
-    println!("");
-    read_booklist();
     let mut borrower_name = String::new();
 
     println!("Enter borrower name: ");
@@ -17,7 +15,7 @@ pub fn borrow_book() {
             Ok(s) => {
                 let temp: Vec<&str> = s.split("\r\n").collect();
                 if temp.iter().any(|&i| i == borrower_name.trim()) {
-                    old_borrower()
+                    old_borrower(borrower_name.trim())
                 } else {
                     new_borrower()
                 }
@@ -29,8 +27,25 @@ pub fn borrow_book() {
     }
 }
 
-fn old_borrower() {
-    println!("Old borrower");
+fn old_borrower(borrower_name: &str) {
+    match fs::read_to_string(format!("members/{}.txt", borrower_name)) {
+        Err(why) => panic!("Error: {}", why),
+        Ok(s) => {
+            let temp: Vec<&str> = s.split("\n").collect();
+
+            let mut book_id = String::new();
+            println!("");
+            read_booklist();
+            println!("Enter book id: ");
+            read_input(&mut book_id);
+
+            for elem in temp {
+                let temp2: Vec<&str> = elem.split(",").collect();
+
+                println!("{:?}", temp2);
+            }
+        }
+    }
 }
 
 fn new_borrower() {
