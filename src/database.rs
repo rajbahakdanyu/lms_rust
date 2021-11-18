6c54b1d9d4a5dcada0f754f3062f801c0889aaf2
danyu rajbahak <rajbahakdanyu@gmail.com>
2021-11-18 20:21:16 +0545

    Added updated booklist update function for return

--- src/database.rs
+++ src/database.rs
@@ -5,16 +5,16 @@ use crate::utils::get_booklist;
 pub fn database(return_type: String, book: Vec<&str>) {
     let list = get_booklist();
     let booklist: Vec<&str> = list.trim().split("\r\n").collect();
-    if return_type == "b" {
-        let mut file = OpenOptions::new()
-            .write(true)
-            .truncate(true)
-            .open("booklist.txt")
-            .unwrap();
+    let mut file = OpenOptions::new()
+        .write(true)
+        .truncate(true)
+        .open("booklist.txt")
+        .unwrap();
 
+    if return_type == "b" {
         for elem in booklist {
             let temp: Vec<&str> = elem.trim().split(",").collect();
-            println!("{:?}", temp);
+
             if temp[0] == book[0] {
                 file.write(
                     format!(
@@ -41,6 +41,33 @@ pub fn database(return_type: String, book: Vec<&str>) {
         }
         println!("{} borrowed successfully\n", book[1]);
     } else if return_type == "r" {
-        println!("user is returning\n")
+        for elem in booklist {
+            let temp: Vec<&str> = elem.trim().split(",").collect();
+            println!("{:?}", temp);
+            if temp[0] == book[0] {
+                file.write(
+                    format!(
+                        "{},{},{},{},{}\n",
+                        temp[0],
+                        temp[1],
+                        temp[2],
+                        &(book[3].to_string().parse::<i32>().unwrap() + 1).to_string(),
+                        temp[4],
+                    )
+                    .as_bytes(),
+                )
+                .unwrap();
+            } else {
+                file.write(
+                    format!(
+                        "{},{},{},{},{}\n",
+                        temp[0], temp[1], temp[2], temp[3], temp[4],
+                    )
+                    .as_bytes(),
+                )
+                .unwrap();
+            }
+        }
+        println!("{} returned successfully\n", book[1]);
     }
 }
